13350581582d8f16b0a38a97d64316382354e115
DekuLiuTesla <1369939686@qq.com>
2025-09-29 20:39:55 +0800

    updates scripts

--- scripts/vggt_colmap_ba.sh
+++ scripts/vggt_colmap_ba.sh
@@ -6,16 +6,18 @@ get_available_gpu() {
 }
 
 dir="data/MipNeRF360"
-post_fix="_vggt_batched_bf16_low_ram"
+post_fix="_ba"
 
 declare -a scenes=(
-    "$dir$post_fix/bicycle"
-    "$dir$post_fix/bonsai"
-    "$dir$post_fix/counter"
-    "$dir$post_fix/garden"
-    "$dir$post_fix/kitchen"
-    "$dir$post_fix/room"
-    "$dir$post_fix/stump"
+    # "$dir/bicycle"
+    # "$dir/bonsai"
+    # "$dir/counter"
+    # "$dir/flowers"
+    # "$dir/garden"
+    # "$dir/kitchen"
+    # "$dir/room"
+    "$dir/stump"
+    "$dir/treehill"
 )
 
 for scene_dir in $dir/*; do
@@ -24,21 +26,22 @@ for scene_dir in $dir/*; do
         gpu_id=$(get_available_gpu)
         if [[ -n $gpu_id ]]; then
             echo "GPU $gpu_id is available. Start running vggt on '$scene_dir'"
-            CUDA_VISIBLE_DEVICES=$gpu_id python demo_colmap_ratio_opt.py \
+            CUDA_VISIBLE_DEVICES=$gpu_id python ba_test.py \
                 --scene_dir $scene_dir \
-                --post_fix $post_fix \
-                --shared_camera \
-                --use_opt & \
+                --post_fix $post_fix & \
+                # --shared_camera \
+                # --use_ba & \
             # Allow some time for the process to initialize and potentially use GPU memory
-            sleep 60
+            sleep 30
             break
         else
             echo "No GPU available at the moment. Retrying in 2 minute."
-            sleep 60
+            sleep 30
         fi
     done
 done
 wait
 
-python utils/gather_results.py $dir$post_fix --format_float --result_filename vggt_results.txt
-# python utils/avg_metrics.py --output_dirs $dir$post_fix/* --save_path $dir$post_fix/vggt_results.txt
+python utils/avg_metrics.py --output_dirs $dir$post_fix/* --save_path $dir$post_fix/results.txt --result_file results.txt
+
+# python utils/avg_metrics.py --output_dirs $scenes --save_path $output_dir/vggt_results.txt
